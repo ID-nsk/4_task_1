8833111a297282f24699d8928bbc14a1795665f7
Ivan Dyakov <i.dyakov@sibstrin.ru>
2021-11-29 14:39:19 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа вычисляет квадраты всех целых чисел от 1 до N");
+            // Ввод данных
+            Console.WriteLine("Введите число N");
+            int N = Convert.ToInt32(Console.ReadLine());
+            // Решение
+            if (N >= 1)
+            {
+                int pow = 1;
+                for (int n = 1, i = 0; N + 1 > n; i = pow, n++)
+                {
+                    pow = i + 2 * n - 1;
+                    Console.WriteLine("Квадрат числа {0} равен {1}", n, pow);
+                }
+                Console.WriteLine("Для выхода нажмите любую клавишу");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Введенное значение вне диапазона");
+                Console.WriteLine("Для выхода нажмите любую клавишу");
+                Console.ReadKey();
+            }
+        }
+    }
+}
